bb59d43b86832795b2189cb42c373c5c2d4b5e80
Victor Oliveira <103453296+Victor23HD@users.noreply.github.com>
2023-03-23 20:31:15 -0300

    Express Init (#2)
    * Express Init
    
    making the initial settings for express.
    
    * postman and update app.js
    
    * Evolving the routes
    
    Adding more HTTP methods, Get and Put.
    
    * Finish basic routes
    
    Creating the delete method
    
    ---------
    
    Co-authored-by: Victor Oliveira <121132421+VictorOliveira233@users.noreply.github.com>

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import Express from "express";
+
+const app = Express();
+
+app.use(Express.json());
+
+const books = 
+[
+    {id: 1, "title": "Lord of the Rings"},
+    {id: 2, "title": "The Hobbit"}
+]
+
+app.get('/',(req,res) => {
+    res.status(200).send("Curso de NodeJs");
+});
+
+app.get('/books',(req, res) => {
+    res.status(200).json(books);
+});
+
+app.get('/books/:id', (req,res) => {
+    let i = findBook(req.params.id);
+    res.status(200).json(books[i]);
+});
+
+app.post('/books', (req,res) => {
+    books.push(req.body);
+    res.status(201).send("O livro foi cadastrado com sucesso!");
+});
+
+app.put('/books/:id', (req,res) => {
+    let i = findBook(req.params.id);
+    books[i].title = req.body.title;
+    res.status(200).json(books);
+});
+
+app.delete('/books/:id', (req, res) => {
+    let i = findBook(req.params.id);
+    books.splice(i, 1);
+    res.status(200).send(`Livro ${i+1} excluido com sucesso!`);
+})
+
+
+const findBook = (id) => 
+{
+    return books.findIndex(book => book.id == id);
+}
+
+export default app;
