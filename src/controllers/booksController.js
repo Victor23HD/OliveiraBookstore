c4ddebc6fd1dbfa588cb5a21c193448c1ebfdcef
Victor Oliveira <121132421+VictorOliveira233@users.noreply.github.com>
2023-04-06 10:28:02 -0300

    Refactor(book/route): Changing class to const

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,37 +1,35 @@
 import books from "../models/Book.js";
 
-class BookController {
+const booksController = {
 
-    static listBooks = async (_, res) => {
-        const book = await books.find()
+    listBooks: async(_, res) => {
+        const book = await books.find().populate('Author');
+        res.status(200).json(book);
+    },
+
+    findByPublisher: async(req, res) => {
+        const book = await books.find({ 'Publisher': req.query.publisher })
         .populate('Author');
         res.status(200).json(book);
-    }
-
-    static registerBook = async(req, res) => {
+    },
+    
+    registerBook: async(req, res) => {
         const book = new books(req.body);
         await book.save();
-        res.status(200).send("Successfully created book!");   
-    }
+        res.status(200).send("Successfully created book!");
+    },
 
-    static updateBook = async(req, res) => {
+    updateBook: async(req, res) => {
         const id = req.params.id;
-        await books.findByIdAndUpdate(id, {$set: req.body});
+        await books.findByIdAndUpdate(id, { $set: req.body });
         res.status(200).send("Successfully updated book!");
-    }
+    },
 
-    static deleteBook = async(req, res) => {
+    deleteBook: async(req, res) => {
         const id = req.params.id;
         await books.findByIdAndDelete(id);
         res.status(200).send("Book successfully deleted!");
     }
-
-    static findByPublisher =  async(req, res) => {
-        const publisher = req.query.publisher;
-        const book = await books.find({'Publisher': publisher});
-        res.status(200).json(book);
-    }
 }
 
-
-export default BookController;
+export default booksController;
